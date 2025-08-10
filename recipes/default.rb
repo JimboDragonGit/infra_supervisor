764bfae56ecdb0d77d60179e5809855937fbf69e
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2025-08-10 14:32:53 -0400

    Using chef_server property

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2023, The Authors, All Rights Reserved.
 
 infra_userdata node[cookbook_name]['bootstrappeur'] do
-  chef_server_url(
+  chef_server(
     {
       chef_server_url: node['chefserver']['chef_server_url'],
       options: {
